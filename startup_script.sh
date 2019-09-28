a20733fa570150e22e9dfa6e3dd068c1c5466e22
Dmitry Kolbin <kolbindmitry@gmail.com>
2019-09-29 03:24:37 +0500

    HomeWork(1.6): add startup, install and deploy scripts

---
+++ startup_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O https://raw.githubusercontent.com/Otus-DevOps-2019-08/DimaK888_infra/cloud-testapp/install_ruby.sh | bash
+wget -O https://raw.githubusercontent.com/Otus-DevOps-2019-08/DimaK888_infra/cloud-testapp/install_mongodb.sh | bash
+wget -O https://raw.githubusercontent.com/Otus-DevOps-2019-08/DimaK888_infra/cloud-testapp/deploy.sh | bash
+
