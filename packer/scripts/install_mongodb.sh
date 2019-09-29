1e7966f6335005bfea4f09a50f6e23df2769143d
Dmitry Kolbin <kolbindmitry@gmail.com>
2019-09-29 12:37:35 +0500

    HomeWork(1.7): add packer template

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+apt update
+apt install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
+
